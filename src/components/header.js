2541c1642b97d40b098e97e0a28612a89e5cfeb1
unknown <ajayibcd@gmail.com>
2022-09-05 03:05:23 -0700

    added layout

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import  './header.css'
+
+export default function home() {
+  return (
+    <div className="header-section">
+       <div className="nav-section">
+            {/* <nav class="flex justify-center space-x-4">
+                <a href="/dashboard" className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
+                <a href="/team" className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Team</a>
+                <a href="/projects" className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Projects</a>
+                <a href="/reports" className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Reports</a>
+            </nav> */}
+            {/* <p> welcome to world class product station</p> */}
+       </div>
+    </div>
+  )
+}
